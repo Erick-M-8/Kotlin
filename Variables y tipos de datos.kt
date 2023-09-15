3e8453bff7d3a1a1a8b5145e8f95037b57cdea1e
Erick-M-8 <144590165+Erick-M-8@users.noreply.github.com>
2023-09-15 16:59:49 -0600

    Add files via upload

---
+++ Variables y tipos de datos.kt
@@ -0,0 +1,10 @@
+const val decimal = 10.25f
+const val pi = 3.1416f
+
+fun main() {
+    //print(decimal::class)
+    var perimetro: Float = 0.0f
+    perimetro = pi*(2.0f*decimal)
+    print("El perímetro del círculo es: $perimetro ")
+}
+
