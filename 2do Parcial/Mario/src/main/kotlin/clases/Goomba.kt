fde7bd61cabd57f8a5f33052c8872a563df96214
unknown <montoya.bonilla.erick.com>
2023-11-22 19:23:05 -0600

    delete

--- 2do Parcial/Mario/src/main/kotlin/clases/Goomba.kt
+++
@@ -1,7 +0,0 @@
-package clases
-
-class Goomba(name: String, strength: Int): Enemy(name,strength) { //Goomba heredará las propiedades y métodos de Enemy.
-    init {
-        println("Iniciando subclase de $name")
-    }
-}
